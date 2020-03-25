8a4a41c13ec2a64647a166d9958503440f3b29f0
marcomonfiglio <44005213+marcomonfiglio@users.noreply.github.com>
2020-03-25 15:40:01 -0700

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,1 +1,14 @@
-hi
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+DEFAULT_PORT = 8000
+
+def runServer(port):
+   CGIHTTPRequestHandler.cgi_directories = ['/scripts']
+   server = HTTPServer(("", port), CGIHTTPRequestHandler)
+   try:
+       server.serve_forever()
+   except KeyboardInterrupt:
+       print("Shutting down server.")
+
+
+runServer(DEFAULT_PORT)
