1ca146d157635c2b0b981f4c87b3bdcff906afbe
marcomonfiglio <44005213+marcomonfiglio@users.noreply.github.com>
2020-03-25 15:41:15 -0700

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -1,1 +1,16 @@
-HI
+#!/usr/bin/env python
+
+import time
+print("Content-Type: text/html\n\n")  # html markup follows
+
+timeStr = time.strftime("%c") # obtains current time
+
+htmlFormat = """
+<html>
+ <Title>The Time Now</Title>
+<body>
+ <p>The current date and time is:  {timeStr}</p>
+</body>
+</html> """
+
+print(htmlFormat.format(**locals()))  # see {timeStr} embedded above
